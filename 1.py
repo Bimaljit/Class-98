fb168ce635b9feb0d824829ba6bf31df00016b5b
Bimaljit Singh Ghuman <74600941+Bimaljit@users.noreply.github.com>
2022-07-23 20:58:07 +0530

    Add files via upload

---
+++ 1.py
@@ -0,0 +1,41 @@
+# Declaring an empty list
+list1 = []
+#  create a list of Numbers
+list1 = [90, 95, 92, 97, 99]
+print(list1)
+
+# checking Type
+print(type(list1))
+
+# length of a list
+print(len(list1))
+
+# a list of strings
+list2 = ["Hi", "Hello", "Hola"]
+print(list2)
+print(len(list2))
+
+# Index of an element
+print(list1.index(95))
+
+# Accessing list element using Positive Index
+print(list1[0])
+
+# Accessing list element using Negative Index
+print(list1[-1])
+
+print(list2[-1])
+
+# Accessing list element using Slice Operator
+print(list1[0:5])
+
+print(list2[0:5])
+
+print(list1[0:3])
+
+print(list2[0:3])
+
+print(list1[-5:-3])
+
+print(list2[-4:-2])
+
---
+++ 2.py
@@ -0,0 +1,26 @@
+# Declaring an empty list
+list1 = []
+#  create a list of Numbers
+list1 = [90, 95, 92, 97, 99]
+print(list1)
+
+list2 = ["Hi", "Hello", "Hola"]
+print(list2)
+
+for elem in list1:
+    print(elem)
+
+for elem in list2:
+    print(elem)
+
+# Range of numbers
+print(range(0,5))
+
+for i in range(0,5):
+    print(i)
+
+for i in range(0,5,2):
+    print(i)
+
+for i in range(5,0,-1):
+    print(i)
